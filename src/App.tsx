ac526bf5fefb2710225665e89b0b889a85463762
ianSama0217 <ian20000217@gmail.com>
2024-09-20 13:47:39 +0800

    slider component

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,25 @@ import "../public/scss/index.scss";
 import Slider from "./components/slider/Slider";
 
 function App() {
+  const [sliderValue, setSliderValue] = useState(20);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = Number(e.target.value);
+    setSliderValue(newValue);
+    // console.log("value", sliderValue);
+  };
+
   return (
     <>
       <h1>App Home</h1>
-      <Slider />
+      <Slider
+        min={0}
+        max={100}
+        step={5}
+        labelText={"評分"}
+        value={sliderValue}
+        handleChange={handleChange}
+      />
     </>
   );
 }
--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,9 +1,38 @@
-type param = {};
+type param = {
+  min?: number;
+  max?: number;
+  step?: number;
+  value?: number;
+  labelText?: string;
+  labelFor?: string;
+  sliderId?: string;
+  handleChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
 
-function Slider() {
+function Slider({
+  min = 0,
+  max = 100,
+  step = 10,
+  value,
+  labelText,
+  labelFor,
+  sliderId = labelFor,
+  handleChange = () => {},
+}: param) {
   return (
     <div className="slider">
-      <input type="range" />
+      <label htmlFor={labelFor}>{labelText}</label>
+      <input
+        type="range"
+        className="primary"
+        id={sliderId}
+        min={min}
+        max={max}
+        step={step}
+        value={value}
+        onChange={handleChange}
+      />
+      <span>{value}</span>
     </div>
   );
 }
